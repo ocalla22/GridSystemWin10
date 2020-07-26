65884ff041c177766aee02ddd970ac370ee17581
Andy <andrew.ocallaghan@ucdconnect.ie>
2020-07-26 02:19:16 +0100

    build and cleanup are working, clean and can be run from anywhere within the project

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,22 @@
 #Assumes we are somewhere and sets project dir as the root of the repo
 project_dir=`git rev-parse --show-toplevel`
 
-#defines path to requirements file
-requirements=$project_dir/requirements.txt
-
-#defines path to main script to be packaged.
-main_script=`find $project_dir -iname hello.py`
-
-#defines path to dist folder
-dist_path=$project_dir/dist
-
-#defines virtual environment name
-venv_path=$project_dir/myvenv
-
 #Creates Virtual environment if it does not already exist
-if [ ! -d "$venv_path" ]; then
-    python -m venv $project_dir/myvenv  
+if [ ! -d "$project_dir/myvenv" ]; then
+    python -m venv $project_dir/myvenv 
 fi
 
 #Activates virtual environment and installs requirements
 . $project_dir/myvenv/Scripts/activate
-pip install -r $requirements
+pip install -r $project_dir/requirements.txt
+
+#defines path to main script to be packaged.
+main_script=`find $project_dir -iname hello.py`
 
 #packages the main file and builds dist folder in project directory.
-pyinstaller --distpath $dist_path $main_script
+pyinstaller \
+    --distpath $project_dir/dist \
+    --workpath $project_dir/build\
+    --specpath $project_dir \
+    --clean \
+    $main_script
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,9 @@
-dirname "$0"
-rm -rf dist/ build/
-rm hello.spec
+#Assumes we are somewhere and sets project dir as the root of the repo
+project_dir=`git rev-parse --show-toplevel`
+
+#Removes dist and build folders.
+rm -r $project_dir/dist/ \
+      $project_dir/build/ 
+
+#Removes .spec file
+rm $project_dir/hello.spec
