cd1e9e79629f15a20cca3fd7a719c83de485654b
Andy <andrew.ocallaghan@ucdconnect.ie>
2020-07-29 02:55:00 +0100

    create file upload button and store files

--- kartinggrid/hello.py
+++ kartinggrid/hello.py
@@ -1,10 +1,19 @@
 
+import tkinter as tk
+from tkinter import filedialog
+
+def UploadAction(event=None):
+    filename = filedialog.askopenfilename()
+    print('Selected:', filename)
+
 def hello():
     return "Hello World"
 
 if __name__ == '__main__':
     import tkinter as tk
     window = tk.Tk()
+    button = tk.Button(window, text='Open', command=UploadAction)
+    button.pack()
     label = tk.Label(text=hello())
     label.pack()
     window.mainloop()
