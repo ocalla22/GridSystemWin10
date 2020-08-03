00e1477176ec38efc50e1f8fb4f0bdfcc7904501
Andy <andrew.ocallaghan@ucdconnect.ie>
2020-08-03 20:25:18 +0100

    requirements updated to included pandas and dependencies

--- kartinggrid/hello.py
+++ kartinggrid/hello.py
@@ -1,24 +1,31 @@
-
-import tkinter as tk
 from tkinter.filedialog import askopenfiles
 from tkinter import Button
+from kartinggrid.timing_files import BasicTimingFile
 
-class loadedTimingData:
-    def __init__(self, data=None):
-        self.data = data
 
-    def set_data(self, data):
-        self.data = data
+def log_contents_of(files):
+    for file in files:
+        for line in file:
+            print(line)
+
+def update_files():
+    files = askopenfiles()
+    log_contents_of(files)
+
+def pack_button(master):
+    upload_settings = dict(
+        master=master,
+        text="add data",
+        command=lambda: update_files()
+    )
+    Button(**upload_settings).pack()
+
 
 if __name__ == '__main__':
     import tkinter as tk
     window = tk.Tk()
+    btf = BasicTimingFile(9)
+    print(btf.data)
 
-    timing_data = TimingData()
-    button_settings = dict(parent=window,
-                           text="click me",
-                           command=lambda : timing_data.set_data(askopenfiles())
-    )
-    btn=Button(upload_button_settings)
-    btn.pack()
+    pack_button(window)
     window.mainloop()
---
+++ kartinggrid/timing_files.py
@@ -0,0 +1,29 @@
+from pandas import read_html
+
+class TimingFileInterface:
+
+    def __init__(self, file=None):
+        self.source_file = file
+        self.data = self.build_data()
+        pass
+
+    def build_data(self):
+        pass
+
+
+class BasicTimingFile(TimingFileInterface):
+
+    def __init__(self, file=None):
+        super().__init__(file)
+
+    def build_data(self):
+        return 1
+
+
+class HTMLTimingFile(TimingFileInterface):
+
+    def __init__(self, file):
+        super().__init__(file)
+
+    def build_data():
+        read_html(super().source_file)
