4ac605b209c8aa05b5006842c3d7bf72d1e44970
Andy O'Callaghan <andrew.ocallaghan@ucdconnect.ie>
2020-10-12 23:10:45 +0100

    build.sh now have a flag modifier so that it can run in pycharm easily. It's still simple to run via command line.
    moving name to app.py, there are 'main.py' files in the environment. So this is less ambiguous. readme updated to reflect changes.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,37 @@
+#Signals to activate venv. Pycharm can't activate venvs in scripts.
+#https://youtrack.jetbrains.com/issue/PY-21974
+activate_venv=true
+
+print_expected_usage() {
+  printf "Usage: ..."
+}
+
+while getopts 'a' flag; do
+  case "${flag}" in
+    a) activate_venv='' ;;
+    *) print_expected_usage
+       exit 1 ;;
+  esac
+done
+
 #Assumes we are somewhere in the project and sets project dir as the root of the repo
 project_dir=`git rev-parse --show-toplevel`
 
 #Creates Virtual environment if it does not already exist
 if [ ! -d "$project_dir/myvenv" ]; then
-    python -m venv $project_dir/myvenv 
+    python -m venv $project_dir/myvenv
 fi
 
-#Activates virtual environment and installs requirements. Hide 'already satisfied' to keep logs clean.
-source $project_dir/myvenv/Scripts/activate
-pip install -r $project_dir/requirements.txt \
-| grep -v 'already satisfied'
+#Activate venv if flag is set.
+if [ $activate_venv ]; then
+    . $project_dir/myvenv/Scripts/activate
+fi
+
+#Installs requirements but hide 'already satisfied' to keep logs clean.
+pip install -r $project_dir/requirements.txt | grep -v 'Requirement already satisfied'
 
 #defines path to main script to be packaged.
-main_script=`find $project_dir -iname main.py`
+main_script=`find $project_dir -iname app.py`
 
 #packages the main file and builds dist folder in project directory.
 pyinstaller \
